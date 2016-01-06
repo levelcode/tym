9a81bf3e9b19f185c5cb212664d8d2fa55aee367
Andres Beltran <abeltran@progracol.com>
2016-01-06 07:58:18 -0500

    Borrado de imagenes de preueba

--- recursos/php/html/inc/catalogo-accesorios.php
+++ recursos/php/html/inc/catalogo-accesorios.php
@@ -16,8 +16,36 @@
 						</section>
 					</div>
 					<div class="col-sm-2 text-center">
-						<h5 class="text-uppercase">Te puede interesar</h5>
+						<h5 class="text-uppercase txt-18 c-color4">Te puede interesar</h5>
 						<i class="st-separador"></i>
+
+						<div class="tipo">
+							<div class="cuadro">
+								<span>Tanques</span>
+							</div>
+							<div class="foto">
+								<img src="recursos/img/img-tanques.jpg" alt="" class="img-responsive">
+							</div>
+							<a href="" class="btn text-uppercase btn-info c-blanco">Comprar<br>Ahora</a>
+						</div>
+						<div class="tipo">
+							<div class="cuadro">
+								<span>Racks</span>
+							</div>
+							<div class="foto">
+								<img src="recursos/img/img-racks.jpg" alt="" class="img-responsive">
+							</div>
+							<a href="" class="btn text-uppercase btn-info c-blanco">Comprar<br>Ahora</a>
+						</div>
+						<div class="tipo">
+							<div class="cuadro">
+								<span>Bicicleteros</span>
+							</div>
+							<div class="foto">
+								<img src="recursos/img/img-bicicleteros.jpg" alt="" class="img-responsive">
+							</div>
+							<a href="" class="btn text-uppercase btn-info c-blanco">Comprar<br>Ahora</a>
+						</div>
 					</div>
 				</div>
 			</div>

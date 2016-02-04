48582bf3cfdc4c15b8da1ec25d7e8b23bcf4e173
Adrian Romero <aromero@progracol.com>
2016-02-04 11:52:28 -0500

    add ctrl to new menu

--- recursos/php/html/inc/menu-accesorios.php
+++ recursos/php/html/inc/menu-accesorios.php
@@ -7,25 +7,25 @@
 			<a href="./llantas" class="llantas" ng-click="openProductType('tire')" data-nombre="accesorios-llantas">Llantas</a>
 		</li>
 		<li>
-			<a href="./portaequipajes" class="portaequipajes" ng-click="openProductType('seat')" data-nombre="accesorios-portaequipajes">Portaequipajes</a>
+			<a href="./portaequipajes" class="portaequipajes" ng-click="openProductType('portaequipajes')" data-nombre="accesorios-portaequipajes">Portaequipajes</a>
 		</li>
 		<li>
-			<a href="./racks" class="racks" ng-click="openProductType('rack')" data-nombre="accesorios-racks">Barras de Techo</a>
+			<a href="./racks" class="racks" ng-click="openProductType('barrastecho')" data-nombre="accesorios-racks">Barras de Techo</a>
 		</li>
 		<li>
-			<a href="./bicicleteros" class="bicicleteros" ng-click="openProductType('rack')" data-nombre="accesorios-bicicleteros">Bicicleteros</a>
+			<a href="./bicicleteros" class="bicicleteros" ng-click="openProductType('bicicleteros')" data-nombre="accesorios-bicicleteros">Bicicleteros</a>
 		</li>
 		<li>
-			<a href="./parrilas-para-techo" class="parrilas-para-techo" ng-click="openProductType('rack')" data-nombre="accesorios-parrilas-para-techo">Parrillas para Techo</a>
+			<a href="./parrilas-para-techo" class="parrilas-para-techo" ng-click="openProductType('parrillastecho')" data-nombre="accesorios-parrilas-para-techo">Parrillas para Techo</a>
 		</li>
 		<li>
-			<a href="./4-4" class="parrilas-4-4" ng-click="openProductType('rack')" data-nombre="accesorios-4-4">Accesorios 4x4</a>
+			<a href="./4-4" class="parrilas-4-4" ng-click="openProductType('accesorios4x4')" data-nombre="accesorios-4-4">Accesorios 4x4</a>
 		</li>
 		<li>
-			<a href="./bomper-estribos" class="parrilas-bomper-estribos" ng-click="openProductType('rack')" data-nombre="accesorios-bomper-estribos">Bomber y Estribos</a>
+			<a href="./bomper-estribos" class="parrilas-bomper-estribos" ng-click="openProductType('')" data-nombre="accesorios-bomper-estribos">Bomber y Estribos</a>
 		</li>
 		<li>
-			<a href="./accesorios" class="accesorios"  ng-click="openProductType('accesorie')" data-nombre="accesorios-accesorios">Accesorios</a>
+			<a href="./accesorios" class="accesorios"  data-nombre="accesorios-accesorios">Accesorios</a>
 		</li>
 		<li>
 			<a href="./luces-hid" class="luces-hid" ng-click="openProductType('light_hid')" data-nombre="accesorios-luces-hid">Luces</a>
--- server/js/angularApp/controllers/productListCtrl.js
+++ server/js/angularApp/controllers/productListCtrl.js
@@ -2,224 +2,272 @@ tymApp.controller('productListCtrl', ['$scope', '$rootScope', 'ConstantsService'
 
 	$scope.productsLoaded = false;
 	$scope.rinProductsSelected = false;
-	$scope.seatProductsSelected = false;	 
+	$scope.seatProductsSelected = false;
 	$scope.lightProductsSelected = false;
-	 
+
 	$scope.rinEmpty = false;
 	$scope.tireEmpty = false;
 	$scope.seatEmpty = false;
 	$scope.lightEmpty = false;
 	$scope.tankEmpty = false;
-	 
-	/*
-	 
-    listeners
- 
-	*/
-	 
-	$rootScope.$on('vehicle_chaged', function( event, data ){
-	 
-		$scope.selectedCar = data;
-	 
-	});
-	 
 
-	 
+	/*
+
+    listeners
+
+	*/
+
+	$rootScope.$on('vehicle_chaged', function( event, data ){
+
+		$scope.selectedCar = data;
+
+	});
+
+
+
 	$rootScope.$on( ConstantsService.CHANGE_PRODUCTS, function( event, data ) {
  		chooseProductsToShow( data );
 	});
-	 
 
-	 
+
+
 	$rootScope.$on( ConstantsService.PRODUCTS_CHARGED, function( event, data ){
-	 
+
 		//$scope.rinProducts = data;
-	 
+
 		$scope.productsLoaded = true;
-	 
+
 
  		angular.forEach( data, function( value, key ){
-	 
+
 			switch ( key ) {
-	 
+
 				case 'rin_products':
-	 
+
 					$scope.rinProducts = value;
-	 
+
 					if ( value.length == 0){
-	 
+
 						$scope.rinEmpty = true;
- 
+
 					}
 					$scope.rinEmpty = false;
-	 
+
 					break;
-	 
+
 				case 'tire_products':
-	 
+
 					$scope.tireProducts = value;
-	 
+
 					if ( !(value.length > 0) ){
-	 
+
 						$scope.tireEmpty = true;
-	 
+
 					}
 					$scope.tireEmpty = false;
-	 
+
 					break;
-	 
+
 				case 'seat_products':
-	 
+
 					$scope.seatProducts = value;
-	 
+
 					if ( !(value.length > 0) ){
-	 
+
 						$scope.seatEmpty = true;
-	 
+
 					}
 
 					$scope.seatEmpty = false;
-	 
+
 					break;
-	 
+
 				case 'light_hid_products':
-	 
+
 					$scope.lightProducts = value;
- 
+
 					if ( !(value.length > 0) ){
-	 
+
 						$scope.lightEmpty = true;
-	 
+
 					}
 					$scope.lightEmpty = false;
-	 
+
 				case 'tank_products':
-	 
+
 					$scope.tankProducts = value;
-	 
+
 					if ( !(value.length > 0) ){
-	 
+
 						$scope.tankEmpty = true;
-	 
+
 					}
 					$scope.tankEmpty = false;
-	 
+
 					break;
-	 
+
 				default:
-	 
+
 			}
-	 
+
 		});
-	 
 
-	 
+
+
 		chooseProductsToShow( 'rin' );
-	 
-	});
-	 
 
-	 
+	});
+
+
+
 	function chooseProductsToShow( productTypeToShow ) {
-	 
+
 		$scope.productsLoaded = true;
-	 
+
 		switch ( productTypeToShow ) {
-	 
+
 			case 'rin':
 				$scope.rinProductsSelected = true;
 				$scope.seatProductsSelected = false;
 				$scope.tireProductsSelected = false;
 				$scope.lightProductsSelected = false;
 				$scope.tankProductsSelected = false;
+				$scope.portaequipajesProductsSelected = false;
+				$scope.barrasTechoProductsSelected = false;
+				$scope.bicicleterosProductsSelected = false;
+				$scope.parrillastechoProductsSelected = false;
+				$scope.accesorios4x4ProductsSelected = false;
 				break;
-	 
+
 			case 'tire':
 				$scope.tireProductsSelected = true;
 				$scope.seatProductsSelected = false;
 				$scope.rinProductsSelected = false;
 				$scope.lightProductsSelected = false;
 				$scope.tankProductsSelected = false;
+				$scope.portaequipajesProductsSelected = false;
+				$scope.barrasTechoProductsSelected = false;
+				$scope.bicicleterosProductsSelected = false;
+				$scope.parrillastechoProductsSelected = false;
+				$scope.accesorios4x4ProductsSelected = false;
 				break;
-	 
-			case 'seat':
-				$scope.seatProductsSelected = true;
-				$scope.tireProductsSelected = false;
-				$scope.rinProductsSelected = false;
-				$scope.lightProductsSelected = false;
-				$scope.tankProductsSelected = false;
-				break;
-	 
-			case 'light_hid':
-				$scope.lightProductsSelected = true;
-				$scope.seatProductsSelected = false;
-				$scope.tireProductsSelected = false;
-				$scope.rinProductsSelected = false;
-				$scope.tankProductsSelected = false;
-				break;
-	 
-			case 'tank':
-				$scope.tankProductsSelected = true;
-				$scope.lightProductsSelected = false;
-				$scope.seatProductsSelected = false;
-				$scope.tireProductsSelected = false;
-				$scope.rinProductsSelected = false;
-				break;
-	 
-			default:
- 
-				console.log('product type no programmed');
-	 
-				break;
-	 
-		}
- 
-	}
- 
 
-	 
+			case 'portaequipajes':
+				$scope.portaequipajesProductsSelected = true;
+				$scope.seatProductsSelected = false;
+				$scope.tireProductsSelected = false;
+				$scope.rinProductsSelected = false;
+				$scope.lightProductsSelected = false;
+				$scope.tankProductsSelected = false;
+				$scope.barrasTechoProductsSelected = false;
+				$scope.bicicleterosProductsSelected = false;
+				$scope.parrillastechoProductsSelected = false;
+				$scope.accesorios4x4ProductsSelected = false;
+				break;
+
+			case 'barrastecho':
+				$scope.barrasTechoProductsSelected = true;
+				$scope.lightProductsSelected = false;
+				$scope.seatProductsSelected = false;
+				$scope.tireProductsSelected = false;
+				$scope.rinProductsSelected = false;
+				$scope.tankProductsSelected = false;
+				$scope.portaequipajesProductsSelected = false;
+				$scope.bicicleterosProductsSelected = false;
+				$scope.parrillastechoProductsSelected = false;
+				$scope.accesorios4x4ProductsSelected = false;
+				break;
+
+			case 'bicicleteros':
+				$scope.bicicleterosProductsSelected = true;
+				$scope.tankProductsSelected = false;
+				$scope.lightProductsSelected = false;
+				$scope.seatProductsSelected = false;
+				$scope.tireProductsSelected = false;
+				$scope.rinProductsSelected = false;
+				$scope.portaequipajesProductsSelected = false;
+				$scope.barrasTechoProductsSelected = false;
+				break;
+			case 'parrillastecho':
+				$scope.parrillastechoProductsSelected = true;
+				$scope.tankProductsSelected = false;
+				$scope.lightProductsSelected = false;
+				$scope.seatProductsSelected = false;
+				$scope.tireProductsSelected = false;
+				$scope.rinProductsSelected = false;
+				$scope.portaequipajesProductsSelected = false;
+				$scope.barrasTechoProductsSelected = false;
+				$scope.bicicleterosProductsSelected = false;
+				$scope.parrillastechoProductsSelected = false;
+				$scope.accesorios4x4ProductsSelected = false;
+				break;
+			case 'accesorios4x4':
+				$scope.accesorios4x4ProductsSelected = true;
+				$scope.tankProductsSelected = false;
+				$scope.lightProductsSelected = false;
+				$scope.seatProductsSelected = false;
+				$scope.tireProductsSelected = false;
+				$scope.rinProductsSelected = false;
+				$scope.portaequipajesProductsSelected = false;
+				$scope.barrasTechoProductsSelected = false;
+				$scope.bicicleterosProductsSelected = false;
+				$scope.parrillastechoProductsSelected = false;
+				$scope.accesorios4x4ProductsSelected = false;
+				break;
+			default:
+
+				console.log('product type no programmed');
+
+				break;
+
+		}
+
+	}
+
+
+
 	function groupByRinType( data ) {
- 
+
 		var na = [];
-	 
+
 			//na.rines = {};
- 
+
 		angular.forEach(data, function (value, key) {
 
        		na[value.diameter] = value;
-	 
+
   		});
-	 	 
+
 		return na;
-	 
-	}
-	 
 
-	 
+	}
+
+
+
 	$scope.sendToProductDetail = function( product, productType ) {
-	 
+
 		var data = { info: product, type: productType };
-	 
+
 		$rootScope.$broadcast( ConstantsService.VIEW_DETAIL, data);
-	 
+
 	}
-	 
 
-	 
+
+
 }]);
- 
 
-	 
+
+
 tymApp.filter('capitalize', function() {
-	 
+
 	return function(input, scope) {
-	 
+
 	if (input!=null)
-	 
+
 		input = input.toLowerCase();
-	 
+
 		return input.substring(0,1).toUpperCase()+input.substring(1);
- 
+
 	}
 
 });

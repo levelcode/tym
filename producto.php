0c09c02b2b09e268863eeab227863fdd4efb549d
Adrian Romero <aromero@progracol.com>
2016-07-29 09:11:28 -0500

    add extra indo for plumillas

--- producto.php
+++ producto.php
@@ -333,15 +333,42 @@ $cabecero = new html\Cabecero($opciones);
 												<br>
 												<br>
 												<div class="row">
-													<div class="col-xs-7">
+													<div class="col-xs-8">
 														<span class="unidades text-center c-color3">Unidades <b class="c-blanco" ng-bind="selectedProduct.stock"></b></span>
-														<br>
-														<br>
+														<div class="form-group" ng-if="response.subType == 'pijamas para vehiculos'">
+															<label for="pijamaSelectedSize" class="c-blanco">Talla:</label>
+															<select id="pijamaSelectedSize" name ="pijamaSelectedSize" ng-model="pijamaSelectedSize" class="form-control" required>
+																<option>S</option>
+																<option>M</option>
+																<option>MM</option>
+																<option>L</option>
+																<option>XL</option>
+																<option>XXL</option>
+															</select>
+														</div>
+														<div class="form-group" ng-if="response.subType == 'plumillas'">
+															<br>
+															<label for="plumillaSelectedSize" class="c-blanco">Nº de plumilla:</label>
+															<select id="plumillaSelectedSize" name="plumillaSelectedSize" ng-model="plumillaSelectedSize" class="form-control" required>
+																<option>14</option>
+																<option>15</option>
+																<option>16</option>
+																<option>17</option>
+																<option>18</option>
+																<option>19</option>
+																<option>20</option>
+																<option>21</option>
+																<option>22</option>
+																<option>23</option>
+																<option>24</option>
+																<option>26</option>
+															</select>
+														</div>
 														<span class="c-color4" ng-if="selectedProduct.color != undefined">Color:</span> <b ng-if="selectedProduct.color != undefined" class="tamano c-blanco txt-18" >{{selectedProduct.color}}</b><br>
 														<span class="c-color4">Caracteristica:</span> <b class="precio txt-15" ng-bind-html="selectedProduct.detail" ></b><br>
-														<span class="c-color4" ng-if="selectedProduct.instructivo == 'si'">Instructivo:</span> <a ng-if="selectedProduct.instructivo == 'si'" href="admin/recursos/documents/instructivos/{{selectedProductType}}.pdf" target="_blank" class="tamano c-blanco txt-18" >Descargar</a><br>
+														<!-- <span class="c-color4" ng-if="selectedProduct.instructivo == 'si'">Instructivo:</span> <a ng-if="selectedProduct.instructivo == 'si'" href="admin/recursos/documents/instructivos/{{selectedProductType}}.pdf" target="_blank" class="tamano c-blanco txt-18" >Descargar</a><br> -->
 													</div>
-													<div class="col-xs-5 text-right">
+													<div class="col-xs-4 text-right">
 														<br>
 														<br>
 														<span class="c-color3 text-uppercase">Precio</span><br>
@@ -357,6 +384,136 @@ $cabecero = new html\Cabecero($opciones);
 													</div>
 													<div class="col-xs-12 text-right">
 														<button ng-click="addToShoppingCart(selectedProduct.id, selectedProduct.referencie, selectedProduct.referencie, '000000', 1, selectedProduct.referencie, 1, selectedProduct.price, 0, 0, selectedProduct.img, selectedProductType, selectedProduct.size)" class="btn btn-info c-color2 text-uppercase"><i class="fa fa-shopping-cart"></i>&nbsp; Añadir producto</button>
+													</div>
+												</div>
+											</div>
+											<div class="row" >
+												<div class="col-md-12">
+													<br>
+													<div class="alert alert-info bg-color4" ng-if="response.subType == 'pijamas para vehiculos'">
+														<i>Instructivo: La siguiente tabla muestra las medidas de largo, ancho y alto en centimetros de nuestra pijamas.</i>
+														<table class="table table-striped">
+															<thead>
+																<tr>
+																	<th>Talla</th>
+																	<th>Largos</th>
+																	<th>Ancho</th>
+																	<th>Alto</th>
+																</tr>
+															</thead>
+															<tbody>
+																<tr>
+																	<td>S</td>
+																	<td>406 cm</td>
+																	<td>165 cm</td>
+																	<td>124 cm</td>
+																</tr>
+																<tr>
+																	<td>M</td>
+																	<td>432 cm</td>
+																	<td>165 cm</td>
+																	<td>124 cm</td>
+																</tr>
+																<tr>
+																	<td>MM</td>
+																	<td>457 cm</td>
+																	<td>178 cm</td>
+																	<td>124 cm</td>
+																</tr>
+																<tr>
+																	<td>L</td>
+																	<td>483 cm</td>
+																	<td>178 cm</td>
+																	<td>124 cm</td>
+																</tr>
+																<tr>
+																	<td>XL</td>
+																	<td>533 cm</td>
+																	<td>178 cm</td>
+																	<td>124 cm</td>
+																</tr>
+																<tr>
+																	<td>XXL</td>
+																	<td>572 cm</td>
+																	<td>203 cm</td>
+																	<td>124 cm</td>
+																</tr>
+															</tbody>
+														</table>
+													</div>
+													<div class="alert alert-info bg-color4" ng-if="response.subType == 'plumillas'">
+														<i>Instructivo: Para conocer la medida de tus plumillas, basta con medir en centimetros la longitud de la goma y ubica el número de la plumilla en la siguiente tabla:</i>
+														<table class="table table-striped">
+															<thead>
+																<tr>
+																	<th>Nº de Plumilla</th>
+																	<th>Longitud en CM</th>
+																	<th>Longitud en MM</th>
+																</tr>
+															</thead>
+															<tbody>
+																<tr>
+																	<td>14</td>
+																	<td>35 cm</td>
+																	<td>350 MM</td>
+																</tr>
+																<tr>
+																	<td>15</td>
+																	<td>37,5 cm</td>
+																	<td>375 MM</td>
+																</tr>
+																<tr>
+																	<td>16</td>
+																	<td>40 cm</td>
+																	<td>400 MM</td>
+																</tr>
+																<tr>
+																	<td>17</td>
+																	<td>42,5 cm</td>
+																	<td>425 MM</td>
+																</tr>
+																<tr>
+																	<td>18</td>
+																	<td>45 cm</td>
+																	<td>450 MM</td>
+																</tr>
+																<tr>
+																	<td>19</td>
+																	<td>47,5 cm</td>
+																	<td>475 MM</td>
+																</tr>
+																<tr>
+																	<td>20</td>
+																	<td>50 cm</td>
+																	<td>500 MM</td>
+																</tr>
+																<tr>
+																	<td>21</td>
+																	<td>52,5 cm</td>
+																	<td>525 MM</td>
+																</tr>
+																<tr>
+																	<td>22</td>
+																	<td>55 cm</td>
+																	<td>550 MM</td>
+																</tr>
+																<tr>
+																	<td>23</td>
+																	<td>57,5 cm</td>
+																	<td>575 MM</td>
+																</tr>
+																<tr>
+																	<td>24</td>
+																	<td>60 cm</td>
+																	<td>6000 MM</td>
+																</tr>
+																<tr>
+																	<td>26</td>
+																	<td>65 cm</td>
+																	<td>650 MM</td>
+																</tr>
+															</tbody>
+														</table>
 													</div>
 												</div>
 											</div>

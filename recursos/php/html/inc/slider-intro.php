18583d32620d8ba36df0d8b2e9e84375204055db
Adrian Romero <aromero@progracol.com>
2016-09-14 14:53:21 -0500

    add embebed video

--- recursos/php/html/inc/slider-intro.php
+++ recursos/php/html/inc/slider-intro.php
@@ -7,58 +7,58 @@
 						<li data-target="#rotabanner" data-slide-to="1"></li>
 						<li data-target="#rotabanner" data-slide-to="2"></li>
 					</ol>
-					<!-- Wrapper for slides --> 
+					<!-- Wrapper for slides -->
 					<div class="carousel-inner" role="listbox">
 						<div class="item active text-center sld-1">
 							<!-- <img src="recursos/img/sld-1.jpg" alt="..."> -->
 							<div class="container">
 								<div class="row">
 									<div class="col-xs-12">
-										<div class="carousel-caption">
+										<!-- <div class="carousel-caption">
 											<div class="etiqueta">
 												<span class="grande">Rines ultralivianos</span><br>
 												<i class="pequeño">en <b>aluminio y cromados</b></i>
 											</div>
-										</div>
+										</div> -->
 									</div>
 								</div>
 							</div>
-								
+
 						</div>
 						<div class="item text-center sld-2">
 							<!-- <img src="recursos/img/sld-2.jpg" alt="..."> -->
 							<div class="container">
 								<div class="row">
 									<div class="col-xs-12">
-										<div class="carousel-caption">
+										<!-- <div class="carousel-caption">
 											<div class="etiqueta">
 												<span class="grande">Rines ultralivianos</span><br>
 												<i class="pequeño">en <b>aluminio y cromados</b></i>
 											</div>
-										</div>
+										</div> -->
 									</div>
 								</div>
 							</div>
-								
+
 						</div>
 						<div class="item text-center sld-3">
 							<!-- <img src="recursos/img/sld-3.jpg" alt="..."> -->
 							<div class="container">
 								<div class="row">
 									<div class="col-xs-12">
-										<div class="carousel-caption">
+										<!-- <div class="carousel-caption">
 											<div class="etiqueta">
 												<span class="grande">Rines ultralivianos</span><br>
-												<i class="pequeño">en <b>aluminio y cromados</b></i> 
+												<i class="pequeño">en <b>aluminio y cromados</b></i>
 											</div>
-										</div>
+										</div> -->
 									</div>
 								</div>
 							</div>
-										
+
 						</div>
 					</div>
-					<!-- Controls 
+					<!-- Controls
 					<a class="left carousel-control" href="#rotabanner" role="button" data-slide="prev">
 						<div class="flecha">
 							<span class="fa fa-angle-left fa-3x"></span>
